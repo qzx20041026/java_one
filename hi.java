ea08cdf5898e032a2728780f610d40d540ce0db5
qzx <2735332991@qq.xom>
2024-01-16 10:24:02 +0800

    打印每一个数

--- hi.java
+++ hi.java
@@ -2,7 +2,13 @@ import java.util.Scanner;
 public class hi  {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int i=sc.nextInt();
-        System.out.println(i);
+        int i=sc.nextInt(),j;
+        for(j=0;j<3;j++)
+        {
+            System.out.print(i%10);
+            System.out.println('\t');
+            i/=10;
+        }
+
     }
 }
