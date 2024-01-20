bd0d550ef3379422258573f397065a8a8352f843
qzx <2735332991@qq.xom>
2024-01-20 15:02:38 +0800

    数组

--- hi.java
+++ hi.java
@@ -2,9 +2,13 @@ import java.util.Scanner;
 import java.util.Random;
 public class hi  {
     public static void main(String[] args) {
-        Random r=new Random();
-        int i= r.nextInt(100);
-        System.out.println(i);
+        //完整形
+        int a[]=new int[]{11,22,33};
+        int[]b=new int[]{11,22,330};
+        //简化形
+        int []arr={1,2,3};
+        int arr1[]={4,5,6};
+        System.out.println(arr[2]);
     }
 }
 
