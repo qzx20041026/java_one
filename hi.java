1c5919f65d771b034cda5d4b1bf78abd52c43fa4
qzx <2735332991@qq.xom>
2024-01-20 14:38:52 +0800

    rand的格式

--- hi.java
+++ hi.java
@@ -1,15 +1,10 @@
 import java.util.Scanner;
+import java.util.Random;
 public class hi  {
     public static void main(String[] args) {
-        //   &一假全假
-        System.out.println(true&false);
-        System.out.println(true&true);
-       //   |   一真全真
-        System.out.println(true|true);
-        System.out.println(true|false);
-        //  ^  同为假，异为真
-        System.out.println(true^true);
-        System.out.println(true^false);
+        Random r=new Random();
+        int i= r.nextInt(100);
+        System.out.println(i);
     }
 }
 
