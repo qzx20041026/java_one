4f82e7bfecbbbdca3c70997d229024d42ce4ce22
qzx <2735332991@qq.xom>
2024-01-22 19:23:37 +0800

    方法

--- hi.java
+++ hi.java
@@ -1,17 +1,14 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.Random;
 public class hi  {
     public static void main(String[] args) {
-        //完整形
-        int a[]=new int[]{11,22,33};
-        int[] b=new int[]{11,22,33};
-        int[] c=new int[45];
-        //简化形
-        int []arr={1,2,3};
-        int arr1[]={4,5,6};
-        //无法这样 int arr2[5];
-        System.out.println(arr[1]);
-
+        int i=add(1,5);
+        System.out.println(i);
+    }
+    public static int add(int x,int y){
+        return x+y;
     }
 }
+
 
