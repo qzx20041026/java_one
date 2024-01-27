d937ce50d567c13573dd26abacfa7460f918285b
qzx <2735332991@qq.xom>
2024-01-27 16:31:44 +0800

    创建一个类（面对对象）

---
+++ Girl.java
@@ -0,0 +1,11 @@
+public class Girl {
+     String name;
+     int age;
+     String gender;
+     public void sleep() {
+          System.out.println("女朋友在睡觉");
+     }
+     public void eat(){
+               System.out.println("女朋友在吃饭");
+     }
+}
