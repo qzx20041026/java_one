f40d99a7d7ac58998d4c51f2e30e9f9dca585b79
qzx <2735332991@qq.xom>
2024-01-28 13:19:42 +0800

    private的用法

--- Girl.java
+++ Girl.java
@@ -1,8 +1,42 @@
 public class Girl {
-     String name;
-     int age;
-     String gender;
-     public void sleep() {
+
+    //private私有关键字只能在该类里修改
+    private String name;
+    private int age;
+    private String gender;
+
+    //getName 获得名字
+    public void getName(String n){
+        name=n;
+
+    }
+    //putName 返回名字
+    public String putName(){
+        return name;
+    }
+    //getAge 获得年龄
+    public void getAge(int a){
+        if (a>=10&&a<30)
+            age=a;
+        else
+            System.out.println("非法数据");
+    }
+    //putAge 返回年龄
+
+    public int putAge(){
+        return age;
+    }
+    //性别
+    public void getgender (String a) {
+        gender=a;
+    }
+    public String putgender () {
+        return gender;
+    }
+
+
+
+    public void sleep() {
           System.out.println("女朋友在睡觉");
      }
      public void eat(){
--- hi.java
+++ hi.java
@@ -1,29 +1,18 @@
 import java.sql.SQLOutput;
 import java.util.Scanner;
 import java.util.Random;
-public class hi  {
-    public static void  main(String[]age){
-        Girl girl1=new Girl();
-        girl1.name="萌萌";
-        girl1.age=18;
-        girl1.gender="萌妹子";
-        System.out.println(girl1.name);
-        System.out.println(girl1.age);
-        System.out.println(girl1.gender);
+public class hi {
+    public static void main(String[] age) {
+        Girl girl1 = new Girl();
+        girl1.getName("杭杭");
+        girl1.getAge(18);
+        girl1.getgender("女");
+        System.out.println(girl1.putName());
+        System.out.println(girl1.putAge());
+        System.out.println(girl1.putgender());
         girl1.eat();
         girl1.sleep();
 
 
-
-
-        Girl girl2=new Girl();
-        girl2.name="lisa";
-        girl2.age=20;
-        girl2.gender="御姐";
-        System.out.println(girl2.name);
-        System.out.println(girl2.age);
-        System.out.println(girl2.gender);
-        girl1.eat();
-        girl1.sleep();
     }
 }
