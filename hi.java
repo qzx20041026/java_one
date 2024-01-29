4c914d2924e441e557338eab486b9935f79dadee
qzx <2735332991@qq.xom>
2024-01-29 16:27:55 +0800

    构造方法

--- hi.java
+++ hi.java
@@ -3,15 +3,8 @@ import java.util.Scanner;
 import java.util.Random;
 public class hi {
     public static void main(String[] age) {
-        Girl girl1 = new Girl();
-        girl1.getName("杭杭");
-        girl1.getAge(18);
-        girl1.getgender("女");
-        System.out.println(girl1.putName());
-        System.out.println(girl1.putAge());
-        System.out.println(girl1.putgender());
-        girl1.eat();
-        girl1.sleep();
+        Girl girl1 = new Girl("杭",18,"女");
+        System.out.println(girl1.age);
 
 
     }
