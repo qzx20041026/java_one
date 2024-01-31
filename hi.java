8dbfa91ac840b790c490e36df65f0a6f94a4a500
qzx <2735332991@qq.xom>
2024-01-31 22:03:49 +0800

    对象数组

--- hi.java
+++ hi.java
@@ -3,8 +3,25 @@ import java.util.Scanner;
 import java.util.Random;
 public class hi {
     public static void main(String[] age) {
-        Girl girl1 = new Girl("杭",18,"女");
-        System.out.println(girl1.age);
+        Scanner sc=new Scanner(System.in);
+        Car[]arr=new Car[3];
+        for (int i = 0; i <arr.length; i++) {
+            Car c=new Car();
+            System.out.println("请输入车的品牌名");
+            String brand=sc.next();
+            c.setBrand(brand);
+            System.out.println("请输入车的价格");
+            int price=sc.nextInt();
+            c.setPrice(price);
+            System.out.println("请输入车的颜色");
+            String color=sc.next();
+            c.setColor(color);
+            arr[i]=c;
+        }
+        for (int i = 0; i < arr.length; i++) {
+            Car c=arr[i];
+            System.out.println(c.getBrand()+","+c.getPrice()+","+c.getColor());
+        }
 
 
     }
