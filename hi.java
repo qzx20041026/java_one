051b1ab4c6217f1985f9bd80a4140c5547d845ba
qzx <2735332991@qq.xom>
2024-02-04 13:07:24 +0800

    字符串的比较

--- hi.java
+++ hi.java
@@ -3,26 +3,17 @@ import java.util.Scanner;
 import java.util.Random;
 public class hi {
     public static void main(String[] age) {
-        Scanner sc=new Scanner(System.in);
-        Car[]arr=new Car[3];
-        for (int i = 0; i <arr.length; i++) {
-            Car c=new Car();
-            System.out.println("请输入车的品牌名");
-            String brand=sc.next();
-            c.setBrand(brand);
-            System.out.println("请输入车的价格");
-            int price=sc.nextInt();
-            c.setPrice(price);
-            System.out.println("请输入车的颜色");
-            String color=sc.next();
-            c.setColor(color);
-            arr[i]=c;
-        }
-        for (int i = 0; i < arr.length; i++) {
-            Car c=arr[i];
-            System.out.println(c.getBrand()+","+c.getPrice()+","+c.getColor());
-        }
+        //字符串的比较
+        String a="abc";
+        String b=new String("abc");
 
+        System.out.println(a==b);
+        boolean d=a.equals(b);
+        System.out.println(d);
+        //equals不无视大小，equalsIgnoreCase无视大小
+        String c="Abc";
+        boolean e=a.equalsIgnoreCase(c);
 
+        System.out.println(e);
     }
 }
