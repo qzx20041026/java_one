1c5c49319bb7a89994469e73dd21d72c18209298
qzx <2735332991@qq.xom>
2024-02-06 13:30:00 +0800

    替换

--- hi.java
+++ hi.java
@@ -4,8 +4,8 @@ import java.util.Random;
 public class hi {
     public static void main(String[] age) {
       String str="121354";
-        //substring截取
-      String a=str.substring(0,2);
+        //replace替换
+      String a=str.replace("2","*");
         System.out.println(a);
     }
 }
