582c8a8ea2f8eb6e394de40fe9356a4df9c53e50
qzx <2735332991@qq.xom>
2024-02-11 16:03:37 +0800

    集合基础操作

--- hi.java
+++ hi.java
@@ -1,43 +1,39 @@
 import java.sql.SQLOutput;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Random;
 import java.util.StringJoiner;
 
 public class hi {
     public static void main(String[] age) {
-        Scanner sc =new Scanner(System.in);
-        StringBuilder sb=new StringBuilder();
-        String str;
-        while (true) {
-            System.out.println("输入数字");
-            str = sc.next();
-            boolean flag = TF(str);
-            if (flag) {
-                break;
-            } else{
-            System.out.println("数据错误");
-                continue;
-            }
+        ArrayList<String>list=new ArrayList<>();
+        //添加元素
+        list.add("aaa");
+        list.add("aaa");
+        list.add("bbb");
+        list.add("ccc");
+
+        //删除元素
+        /*boolean result1=list.remove("aaa");
+        System.out.println(result1);*/
+
+
+        /*String str=list.remove(2);
+        System.out.println(str);*/
+
+        //修改元素 返回被修改元素
+        /*String str=list.set(1,"ddd");
+        System.out.println(str);*/
+
+
+        //查询元素    size返回长度
+        /*String s=list.get(0);
+        System.out.println(s);*/
+        for (int i = 0; i < list.size(); i++) {
+            String a=list.get(i);
+            System.out.println(a);
         }
-            for (int i = 0; i < str.length(); i++) {
-                int c=str.charAt(i)-48;
-                String x=LuoMa(c);
-                sb.append(x);
-            }
-        System.out.println(sb);
-    }
-    public static boolean TF(String str){
-        for (int i = 0; i <str.length() ; i++) {
-            char c=str.charAt(i);
-            if (c<'0'||c>'9') {
-                return false;
-            }
-        }
-        return true;
-    }
-    public static String LuoMa(int c){
-        String[]arr={"","Ⅰ","Ⅱ","Ⅲ","Ⅳ","Ⅴ","Ⅵ","Ⅶ","Ⅷ","Ⅸ"};
-        return arr[c];
+
     }
 }
 
