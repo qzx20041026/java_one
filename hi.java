5321b95bde5e9e96fa03a05ce86fcb487dfaf452
qzx <2735332991@qq.xom>
2024-02-12 17:08:31 +0800

    StringJoiner

--- hi.java
+++ hi.java
@@ -6,34 +6,9 @@ import java.util.StringJoiner;
 
 public class hi {
     public static void main(String[] age) {
-        ArrayList<String>list=new ArrayList<>();
-        //添加元素
-        list.add("aaa");
-        list.add("aaa");
-        list.add("bbb");
-        list.add("ccc");
-
-        //删除元素
-        /*boolean result1=list.remove("aaa");
-        System.out.println(result1);*/
-
-
-        /*String str=list.remove(2);
-        System.out.println(str);*/
-
-        //修改元素 返回被修改元素
-        /*String str=list.set(1,"ddd");
-        System.out.println(str);*/
-
-
-        //查询元素    size返回长度
-        /*String s=list.get(0);
-        System.out.println(s);*/
-        for (int i = 0; i < list.size(); i++) {
-            String a=list.get(i);
-            System.out.println(a);
-        }
-
+        StringJoiner sj=new StringJoiner(", ","[","]");
+        sj.add("aaa").add("bbb").add("ccc");
+        System.out.println(sj);
     }
 }
 
