abbf5c6a22e416ff248a74756d8c0b245d116c94
qzx <2735332991@qq.xom>
2024-02-12 17:49:43 +0800

    集合和对象练习

--- hi.java
+++ hi.java
@@ -5,10 +5,23 @@ import java.util.Random;
 import java.util.StringJoiner;
 
 public class hi {
-    public static void main(String[] age) {
-        StringJoiner sj=new StringJoiner(", ","[","]");
-        sj.add("aaa").add("bbb").add("ccc");
-        System.out.println(sj);
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        ArrayList<Student>list=new ArrayList<>();
+        for (int i = 0; i <3 ; i++) {
+            Student s=new Student();
+            System.out.println("请输入名字");
+            String name=sc.next();
+            System.out.println("请输入年龄");
+            int age=sc.nextInt();
+            s.setAge(age);
+            s.setName(name);
+            list.add(s);
+        }
+        for (int i = 0; i < list.size(); i++) {
+            Student stu=list.get(i);
+            System.out.println(stu.getName()+", "+stu.getAge());
+        }
     }
 }
 
